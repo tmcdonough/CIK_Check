b42a0035a789c15266c1eca18dd53d71f1729f86
tmcdonough <mcdonough.t@gmail.com>
2013-07-19 09:34:09 -0700

    updated cleaner.py
    major revisions. biggest = no longer stores files in list and then
    exports at end--it now writes to csv as it goes. The former method used
    up too much memory and crashed the computer

--- cleaner.py
+++ cleaner.py
@@ -36,69 +36,77 @@ def construct_link(cik):
     link = part1+cik+part2
     return link
 
-CIKs = csv.reader(open("CIK.csv", "rU"))
-CIKs2 = []
-for i in CIKs:
-    CIKs2.append(i)
+def run_through(cik_list, outfile):
+    reject_count = 0
+    master_count = 0
+    current_count = 0
+    CIKs = csv.reader(open(cik_list, "rU"))
+    CIKs2 = []
+    for i in CIKs:
+        CIKs2.append(i)
+    with open(outfile, "w") as out_file:
+        for i in CIKs2:
+            entry = []
+            curr = time.clock()
+            print "Checking: "+str(i[0])
+            print "*******************"
+            print "*******************"
+            currentct = float(CIKs2.index(i)+1)
+            totalct = float(len(CIKs2))
+            time_remaining_seconds = (((curr-start)/currentct)*(totalct-currentct))
+            time_remaining_hours = (((((curr-start)/currentct)*(totalct-currentct))/60)/60)
+            est_completed = (currentct/totalct)
+            print str(currentct)+" of "+str(totalct)+" CIKs checked..."
+            print str(est_completed*100)+'% of CIKs checked...'
+            print "*******************"
+            print "*******************"
+            print "Estimated time remaining: "+str(time_remaining_hours)+" hours"
+            valid = True
+            page = get_page(construct_link(i))
+            for e in page:
+                if "No matching CIK." in e:
+                    entry.append(["","",i[0]])
+                    valid = False
+                    reject_count+=1
+                    print "     "+str(i[0])+" is invalid ("+str(reject_count)+" total rejected)"
+                    for CURRENT_CIK, MASTER_CIK, REJECTED_CIK in entry:
+                        out_file.write("%s,%s,%s\n" % (CURRENT_CIK, MASTER_CIK, REJECTED_CIK))
+                    entry = []
+                    break
+                else:
+                    continue
+            if valid:
+                #page2 = get_page(construct_link(i))
+                for k in page:
+                    if '<td nowrap=' in k:
+                        position = page.index(k)
+                        for o in range(0,5):
+                            new = page[position+1]
+                            if '<td>' in new:
+                                if '-' in new:
+                                    year = new.split('-')[0]
+                                    year = year.split('>')[1]
+                                else:
+                                    year = new.split('>')[1]
+                                    year = year[:4]
+                                if int(year) >= 2012:
+                                    entry.append([i[0],i[0],""])
+                                    master_count+=1
+                                    current_count+=1
+                                    print "     "+str(i[0])+" is valid and current ("+str(current_count)+" total current)"
+                                else:
+                                    entry.append(["",i[0],""])
+                                    master_count = master_count + 1
+                                    print "     "+str(i[0])+" is valid ("+str(master_count)+" total valid)"
+                                for CURRENT_CIK, MASTER_CIK, REJECTED_CIK in entry:
+                                    out_file.write("%s,%s,%s\n" % (CURRENT_CIK.strip(), MASTER_CIK.strip(), REJECTED_CIK.strip()))
+                                entry = []
+                                break
+                            else:
+                                position +=1
+                        break       
+
 start = time.clock()
-for i in CIKs2:
-    curr = time.clock()
-    print "Checking: "+str(i[0])
-    print "*******************"
-    print "*******************"
-    time_remaining_seconds = (((curr-start)/(CIKs2.index(i)+1))*(len(CIKs2)-(CIKs2.index(i)+1)))
-    time_remaining_hours = (((((curr-start)/(CIKs2.index(i)+1))*(len(CIKs2)-(CIKs2.index(i)+1)))/60)/60)
-    est_completed = ((curr-start)/(time_remaining_seconds))*100
-    print str(CIKs2.index(i)+1)+" of "+str(len(CIKs2))+" CIKs checked..."
-    print str(est_completed)+"% of CIKs checked..."
-    print "*******************"
-    print "*******************"
-    print "Estimated time remaining: "+str(time_remaining_hours)+" hours"
-    valid = True
-    page = get_page(construct_link(i))
-    for e in page:
-        if "No matching CIK." in e:
-            #rejected.append(i)
-            valid = False
-            print "     "+str(i[0])+" is invalid"
-            break
-        else:
-            continue
-    if valid:
-        #page2 = get_page(construct_link(i))
-        for k in page:
-            if '<td nowrap=' in k:
-                position = page.index(k)
-                for o in range(0,5):
-                    new = page[position+1]
-                    if '<td>' in new:
-                        if '-' in new:
-                            year = new.split('-')[0]
-                            year = year.split('>')[1]
-                        else:
-                            year = new.split('>')[1]
-                            year = year[:4]
-                        if int(year) >= 2012:
-                            current.append(i)
-                            master.append(i)
-                            print "     "+str(i[0])+" is valid and current ("+str(len(current))+" total current)"
-                        else:
-                            master.append(i)
-                            print "     "+str(i[0])+" is valid ("+str(len(master))+" total valid)"
-                        break
-                    else:
-                        position +=1
-                break
+run_through("CIK.csv", "master.csv")
 end = time.clock()
 print "runtime: "+str(end-start)
-with open("master_CIK.csv", "w") as out_file:
-    for c in master:
-        out_file.write("%s\n" % (c[0]))
-
-with open("current_CIK.csv", "w") as out_file2:
-    for d in current:
-        out_file2.write("%s\n" % (d[0]))
-
-#with open("rejected_CIK.csv", "w") as out_file3:
- #   for e in rejected:
-  #      out_file3.write("%s\n" % (e[0]))
